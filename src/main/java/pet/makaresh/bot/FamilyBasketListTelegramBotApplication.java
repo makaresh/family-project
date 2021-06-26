245e40ef3abace2ada695538b7b9f8c0ce0fe352
makaresh <MAKareSH12345>
2021-06-26 22:41:41 +0400

    Initial commit

---
+++ src/main/java/pet/makaresh/bot/FamilyBasketListTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pet.makaresh.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBasketListTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyBasketListTelegramBotApplication.class, args);
+    }
+
+}
